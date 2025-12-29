c61084cf2647b94d0172aac83d761b82c75d4ae2
Bintang-xiao <pratamabintang096@gmail.com>
2025-12-29 15:38:19 +0700

    Update portofolio frontend

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const button = document.querySelector("button");
-const text = document.querySelector("#text");
+const toggleTheme = document.querySelector("#toggle-theme");
 
-button.addEventListener("click", () => {
-  text.textContent = "Sedang belajar dan berkembang";
-  text.classList.toggle("active");
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggleTheme.textContent = "Light Mode";
+  } else {
+    toggleTheme.textContent = "Dark Mode";
+  }
 });
